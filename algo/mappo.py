43987b8dd33a794bfb032838830107deb500e299
bacngv <bac17012004@gmail.com>
2025-02-15 20:04:19 +0700

    fix

--- algo/mappo.py
+++ algo/mappo.py
@@ -15,39 +15,30 @@ class MetaBuffer(object):
         self.start = 0
         self.length = 0
         self._flag = 0
-
     def __len__(self):
         return self.length
-
     def __getitem__(self, idx):
         if idx < 0 or idx >= self.length:
             raise KeyError()
         return self.data[idx]
-
     def sample(self, idx):
         return self.data[idx % self.length]
-
     def pull(self):
         return self.data[:self.length]
-
     def append(self, value):
-        start = 0
         num = len(value)
-
         if self._flag + num > self.max_len:
             tail = self.max_len - self._flag
             self.data[self._flag:] = value[:tail]
             num -= tail
-            start = tail
-            self._flag = 0
-
-        self.data[self._flag:self._flag + num] = value[start:]
-        self._flag += num
+            self.data[:num] = value[tail:]
+            self._flag = num
+        else:
+            self.data[self._flag:self._flag + num] = value
+            self._flag += num
         self.length = min(self.length + len(value), self.max_len)
-
     def reset_new(self, start, value):
         self.data[start:] = value
-
 
 class AgentMemory(object):
     def __init__(self, obs_shape, feat_shape, act_n, max_len, use_mean=False, global_state_shape=None):
@@ -59,18 +50,19 @@ class AgentMemory(object):
         self.use_mean = use_mean
         if self.use_mean:
             self.prob = MetaBuffer((act_n,), max_len)
+        self.old_log_prob = MetaBuffer((), max_len)
         self.global_state = MetaBuffer(global_state_shape, max_len)
 
-    def append(self, obs0, feat0, act, reward, alive, prob=None, global_state=None):
+    def append(self, obs0, feat0, act, reward, alive, old_log_prob, prob=None, global_state=None):
         self.obs0.append(np.array([obs0]))
         self.feat0.append(np.array([feat0]))
         self.actions.append(np.array([act], dtype=np.int32))
         self.rewards.append(np.array([reward]))
         self.terminals.append(np.array([not alive], dtype=bool))
+        self.old_log_prob.append(np.array([old_log_prob]))
         if self.use_mean:
             self.prob.append(np.array([prob]))
         self.global_state.append(np.array([global_state]))
-
 
     def pull(self):
         res = {
@@ -79,11 +71,11 @@ class AgentMemory(object):
             'act': self.actions.pull(),
             'rewards': self.rewards.pull(),
             'terminals': self.terminals.pull(),
+            'old_log_prob': self.old_log_prob.pull(),
             'prob': None if not self.use_mean else self.prob.pull(),
             'global_state': self.global_state.pull()
         }
         return res
-
 
 
 class MemoryGroup(object):
@@ -96,13 +88,13 @@ class MemoryGroup(object):
         self.sub_len = sub_len
         self.use_mean = use_mean
         self.act_n = act_n
-
         self.obs0 = MetaBuffer(obs_shape, max_len)
         self.feat0 = MetaBuffer(feat_shape, max_len)
         self.actions = MetaBuffer((), max_len, dtype='int32')
         self.rewards = MetaBuffer((), max_len)
         self.terminals = MetaBuffer((), max_len, dtype='bool')
         self.masks = MetaBuffer((), max_len, dtype='bool')
+        self.old_log_prob = MetaBuffer((), max_len)
         if use_mean:
             self.prob = MetaBuffer((act_n,), max_len)
         self._new_add = 0
@@ -113,16 +105,19 @@ class MemoryGroup(object):
         self.actions.append(kwargs['act'])
         self.rewards.append(kwargs['rewards'])
         self.terminals.append(kwargs['terminals'])
+        self.old_log_prob.append(kwargs['old_log_prob'])
         if self.use_mean:
             self.prob.append(kwargs['prob'])
         mask = np.where(kwargs['terminals'] == True, False, True)
-        mask[-1] = False
+        if len(mask) > 0:
+            mask[-1] = False
         self.masks.append(mask)
 
     def push(self, **kwargs):
         for i, _id in enumerate(kwargs['ids']):
-            if self.agent.get(_id) is None:
-                self.agent[_id] = AgentMemory(self.obs_shape, self.feat_shape, self.act_n, self.sub_len, use_mean=self.use_mean)
+            if _id not in self.agent:
+                self.agent[_id] = AgentMemory(self.obs_shape, self.feat_shape, self.act_n, self.sub_len, use_mean=self.use_mean,
+                                              global_state_shape=kwargs['global_state'][0].shape)
             if self.use_mean:
                 self.agent[_id].append(
                     obs0=kwargs['state'][0][i],
@@ -130,7 +125,9 @@ class MemoryGroup(object):
                     act=kwargs['acts'][i],
                     reward=kwargs['rewards'][i],
                     alive=kwargs['alives'][i],
-                    prob=kwargs['prob'][i]
+                    old_log_prob=kwargs['old_log_prob'][i],
+                    prob=kwargs['prob'][i],
+                    global_state=kwargs['global_state'][i]
                 )
             else:
                 self.agent[_id].append(
@@ -138,7 +135,9 @@ class MemoryGroup(object):
                     feat0=kwargs['state'][1][i],
                     act=kwargs['acts'][i],
                     reward=kwargs['rewards'][i],
-                    alive=kwargs['alives'][i]
+                    alive=kwargs['alives'][i],
+                    old_log_prob=kwargs['old_log_prob'][i],
+                    global_state=kwargs['global_state'][i]
                 )
 
     def tight(self):
@@ -162,6 +161,7 @@ class MemoryGroup(object):
         rewards = self.rewards.sample(idx)
         dones = self.terminals.sample(idx)
         masks = self.masks.sample(idx)
+        old_log_prob = self.old_log_prob.sample(idx)
 
         if self.use_mean:
             act_prob = self.prob.sample(idx)
@@ -169,7 +169,7 @@ class MemoryGroup(object):
         else:
             act_prob = None
             act_next_prob = None
-        return obs, feature, obs_next, feature_next, dones, rewards, actions, masks
+        return obs, feature, obs_next, feature_next, dones, rewards, actions, masks, old_log_prob
 
     def get_batch_num(self):
         print('\n[INFO] Length of buffer and new add:', len(self.obs0), self._new_add)
@@ -181,22 +181,22 @@ class MemoryGroup(object):
     def nb_entries(self):
         return len(self.obs0)
 
+
 class MAMemoryGroup(MemoryGroup):
     def __init__(self, obs_shape, feat_shape, global_state_shape, act_n, max_len, batch_size, sub_len, use_mean=False):
         super().__init__(obs_shape, feat_shape, act_n, max_len, batch_size, sub_len, use_mean)
         self.global_states = MetaBuffer(global_state_shape, max_len)
-
     def _flush(self, **kwargs):
         super()._flush(**kwargs)
         self.global_states.append(kwargs['global_state'])
-
     def push(self, **kwargs):
         n_agents = len(kwargs['ids'])
         global_states_array = np.array([kwargs['global_state']] * n_agents)
         kwargs['global_state'] = global_states_array
         for i, _id in enumerate(kwargs['ids']):
-            if self.agent.get(_id) is None:
-                self.agent[_id] = AgentMemory(self.obs_shape, self.feat_shape, self.act_n, self.sub_len, use_mean=self.use_mean, global_state_shape=self.global_states.data.shape[1:])
+            if _id not in self.agent:
+                self.agent[_id] = AgentMemory(self.obs_shape, self.feat_shape, self.act_n, self.sub_len, use_mean=self.use_mean,
+                                              global_state_shape=self.global_states.data.shape[1:])
             if self.use_mean:
                 self.agent[_id].append(
                     obs0=kwargs['state'][0][i],
@@ -204,6 +204,7 @@ class MAMemoryGroup(MemoryGroup):
                     act=kwargs['acts'][i],
                     reward=kwargs['rewards'][i],
                     alive=kwargs['alives'][i],
+                    old_log_prob=kwargs['old_log_prob'][i],
                     prob=kwargs['prob'][i],
                     global_state=global_states_array[i]
                 )
@@ -214,32 +215,16 @@ class MAMemoryGroup(MemoryGroup):
                     act=kwargs['acts'][i],
                     reward=kwargs['rewards'][i],
                     alive=kwargs['alives'][i],
+                    old_log_prob=kwargs['old_log_prob'][i],
                     global_state=global_states_array[i]
                 )
-
     def sample(self):
-        idx = np.random.choice(self.nb_entries, size=self.batch_size)
-        next_idx = (idx + 1) % self.nb_entries
+        (obs, feat, obs_next, feat_next, dones, rewards,
+         acts, masks, old_log_prob) = super().sample()
+        global_state = self.global_states.sample(np.arange(len(self.obs0))[:self.batch_size])
+        global_state_next = self.global_states.sample((np.arange(len(self.obs0))[:self.batch_size] + 1) % len(self.obs0))
+        return obs, feat, obs_next, feat_next, dones, rewards, acts, masks, old_log_prob, global_state, global_state_next
 
-        obs = self.obs0.sample(idx)
-        obs_next = self.obs0.sample(next_idx)
-        feat = self.feat0.sample(idx)
-        feat_next = self.feat0.sample(next_idx)
-        actions = self.actions.sample(idx)
-        rewards = self.rewards.sample(idx)
-        dones = self.terminals.sample(idx)
-        masks = self.masks.sample(idx)
-
-        if self.use_mean:
-            act_prob = self.prob.sample(idx)
-            act_next_prob = self.prob.sample(next_idx)
-        else:
-            act_prob = None
-            act_next_prob = None
-
-        global_state = self.global_states.sample(idx)
-        global_state_next = self.global_states.sample(next_idx)
-        return obs, feat, obs_next, feat_next, dones, rewards, actions, masks, global_state, global_state_next
 
 # CTDE MAPPO
 class MAPPO(base.ValueNet):
@@ -263,15 +248,12 @@ class MAPPO(base.ValueNet):
                                            memory_size, batch_size, sub_len, use_mean=use_mf)
         self.actor = self._construct_actor()
         self.critic = self._construct_critic()
-
         self.target_actor = self._construct_actor()
         self.target_critic = self._construct_critic()
         self.target_actor.load_state_dict(self.actor.state_dict())
         self.target_critic.load_state_dict(self.critic.state_dict())
-
         self.actor_optim = torch.optim.Adam(self.get_params(self.actor), lr=learning_rate)
         self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=learning_rate)
-
     def get_all_params(self):
         params = []
         for k, v in self.actor.items():
@@ -279,13 +261,11 @@ class MAPPO(base.ValueNet):
         for param in self.critic.parameters():
             params.append(param)
         return params
-
     def get_params(self, network_dict):
         params = []
         for k, v in network_dict.items():
             params.extend(list(v.parameters()))
         return params
-
     def _construct_actor(self):
         temp_dict = nn.ModuleDict()
         temp_dict['conv1'] = nn.Conv2d(in_channels=self.view_space[2], out_channels=32, kernel_size=3)
@@ -315,8 +295,11 @@ class MAPPO(base.ValueNet):
             nn.Linear(128, 1)
         )
         return model
-    def get_value(self, global_state):
-        value = self.critic(global_state)
+    def get_value(self, global_state, use_target=False):
+        if use_target:
+            value = self.target_critic(global_state)
+        else:
+            value = self.critic(global_state)
         return value.squeeze(-1)
     def get_action_and_value(self, obs, feature, prob=None, action=None):
         a_h = F.relu(self.actor['conv2'](F.relu(self.actor['conv1'](obs)))).flatten(start_dim=1)
@@ -328,27 +311,35 @@ class MAPPO(base.ValueNet):
         dist = Categorical(probs)
         if action is None:
             action = dist.sample()
-        value = None  # global state ở critic
         log_prob = dist.log_prob(action)
         entropy = dist.entropy()
+        value = None
         return action, log_prob, entropy, value
-
     def act(self, obs, feature, prob=None, eps=None):
         with torch.no_grad():
             action, _, _, _ = self.get_action_and_value(obs, feature, prob)
         return action.cpu().numpy()
-
     def update(self):
         for param, target_param in zip(self.actor.parameters(), self.target_actor.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
         for param, target_param in zip(self.critic.parameters(), self.target_critic.parameters()):
             target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
-
     def flush_buffer(self, **kwargs):
         global_state = self.get_global_state(kwargs['state'][0], kwargs['state'][1])
         kwargs['global_state'] = global_state
-        self.replay_buffer.push(**kwargs)
+        device = next(self.actor.parameters()).device
+        obs_tensor = torch.FloatTensor(np.array(kwargs['state'][0])).permute(0, 3, 1, 2).to(device)
+        feat_tensor = torch.FloatTensor(np.array(kwargs['state'][1])).to(device)
+        if kwargs.get('prob') is not None:
+            prob_tensor = torch.FloatTensor(np.array(kwargs['prob'])).to(device)
+        else:
+            prob_tensor = None
+        acts_tensor = torch.LongTensor(kwargs['acts']).to(device)
 
+        with torch.no_grad():
+            _, log_prob, _, _ = self.get_action_and_value(obs_tensor, feat_tensor, prob_tensor, action=acts_tensor)
+        kwargs['old_log_prob'] = log_prob.cpu().numpy()
+        self.replay_buffer.push(**kwargs)
     def get_global_state(self, obs_all, feat_all):
         global_list = []
         for o, f in zip(obs_all, feat_all):
@@ -364,15 +355,13 @@ class MAPPO(base.ValueNet):
         elif current_length > expected_length:
             global_state = global_state[:expected_length]
         return global_state
-
-
     def train(self, cuda):
         self.replay_buffer.tight()
         batch_num = self.replay_buffer.get_batch_num()
         total_loss = 0
         for i in range(batch_num):
-            obs, feat, obs_next, feat_next, dones, rewards, acts, masks, global_state, global_state_next = self.replay_buffer.sample()
-
+            sample_data = self.replay_buffer.sample()
+            obs, feat, obs_next, feat_next, dones, rewards, acts, masks, old_log_prob, global_state, global_state_next = sample_data
             obs = torch.FloatTensor(obs).permute([0, 3, 1, 2])
             obs_next = torch.FloatTensor(obs_next).permute([0, 3, 1, 2])
             feat = torch.FloatTensor(feat)
@@ -381,9 +370,9 @@ class MAPPO(base.ValueNet):
             rewards = torch.FloatTensor(rewards)
             dones = torch.FloatTensor(dones)
             masks = torch.FloatTensor(masks)
+            old_log_prob = torch.FloatTensor(old_log_prob)
             global_state = torch.FloatTensor(global_state)
             global_state_next = torch.FloatTensor(global_state_next)
-
             if cuda:
                 obs = obs.cuda()
                 obs_next = obs_next.cuda()
@@ -393,27 +382,30 @@ class MAPPO(base.ValueNet):
                 rewards = rewards.cuda()
                 dones = dones.cuda()
                 masks = masks.cuda()
+                old_log_prob = old_log_prob.cuda()
                 global_state = global_state.cuda()
                 global_state_next = global_state_next.cuda()
             _, new_log_prob, entropy, _ = self.get_action_and_value(obs, feat, action=acts)
-            values = self.get_value(global_state)
+            # r = exp(new_log_prob - old_log_prob)
+            ratio = torch.exp(new_log_prob - old_log_prob)
+            values = self.get_value(global_state, use_target=False)
             with torch.no_grad():
-                next_values = self.get_value(global_state_next)
+                next_values = self.get_value(global_state_next, use_target=True)
+            # gae
             advantages = torch.zeros_like(rewards)
             lastgaelam = 0
             for t in reversed(range(len(rewards))):
                 if t == len(rewards) - 1:
                     nextnonterminal = 1.0 - dones[t]
-                    nextvalues = next_values[t]
+                    nextvalue = next_values[t]
                 else:
                     nextnonterminal = 1.0 - dones[t]
-                    nextvalues = values[t + 1]
-                delta = rewards[t] + self.gamma * nextvalues * nextnonterminal - values[t]
+                    nextvalue = values[t + 1]
+                delta = rewards[t] + self.gamma * nextvalue * nextnonterminal - values[t]
                 lastgaelam = delta + self.gamma * self.gae_lambda * nextnonterminal * lastgaelam
                 advantages[t] = lastgaelam
             returns = advantages + values
             advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
-            ratio = torch.exp(new_log_prob)
             surr1 = ratio * advantages
             surr2 = torch.clamp(ratio, 1 - self.clip_param, 1 + self.clip_param) * advantages
             policy_loss = -torch.min(surr1, surr2).mean()
@@ -429,6 +421,7 @@ class MAPPO(base.ValueNet):
             if i % 50 == 0:
                 print(f'[MAPPO] LOSS: {loss.item():.4f} (Policy: {policy_loss.item():.4f}, '
                       f'Value: {value_loss.item():.4f}, Entropy: {entropy_loss.item():.4f})')
+        print(f"[MAPPO] Total loss: {total_loss:.4f}")
     def save(self, dir_path, step=0):
         os.makedirs(dir_path, exist_ok=True)
         actor_path = os.path.join(dir_path, f"mappo_actor_{step}")
